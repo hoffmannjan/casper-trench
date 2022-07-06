4319e07e66fab5d62e4769e1f5e5169027c08f71
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-06 21:33:29 +0300

    tailwind fixes

--- src/lib/components/Header/OtherStatsSection.svelte
+++ src/lib/components/Header/OtherStatsSection.svelte
@@ -60,10 +60,9 @@
     .home-stats-section {
         @apply h-[clamp(100px,5.5vw,5.5vw)] w-[clamp(500px,46.96vw,46.96vw)];
         @apply py-[clamp(4px,0.48vw,0.48vw)] px-[clamp(20px,1.5vw,1.5vw)] rounded-[clamp(10px,1.19vw,1.19vw)] border-[clamp(1px,0.06vw,0.06vw)] border-color-border-header-stats border-opacity-50;
-        @apply flex gap-[clamp(10px,1.43vw,1.43vw)] items-center;
+        @apply flex gap-[clamp(10px,1.43vw,1.43vw)] items-center backdrop-blur;
 
         background: radial-gradient(37.71% 40.12% at 94.46% 4.58%, rgba(14, 118, 151, 0.32) 0%, rgba(12, 129, 149, 0) 100%), radial-gradient(22.05% 48.04% at 1.06% 41.67%, rgba(20, 71, 159, 0.49) 0%, rgba(19, 78, 158, 0) 100%), linear-gradient(296.24deg, rgba(22, 58, 162, 0.3) 3.67%, rgba(10, 148, 146, 0.3) 98.49%);
-        backdrop-filter: blur(7px);
     }
 
     .stat-column {
