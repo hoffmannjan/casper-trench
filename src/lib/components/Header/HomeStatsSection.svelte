6bd7e22218e0a3442a888ceff78949552f8a7dce
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-06 17:14:06 +0300

    add: home page stats component

---
+++ src/lib/components/Header/HomeStatsSection.svelte
@@ -0,0 +1,128 @@
+<div class="home-stats-section">
+
+    <div class="stat-column">
+        <div class="top">
+            <div class="title">
+                BLOCK HEIGHT
+            </div>
+            <div class="value">
+                {"861,309"}
+            </div>
+            <div class="detail">
+                {"55 sec ago"}
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="title">
+                CSPR PRICE
+            </div>
+            <div class="value">
+                {"$0.0264"}
+            </div>
+            <div class="detail">
+                {"$135,498,998 Market Cap"}
+            </div>
+        </div>
+    </div>
+
+    <div class="vt"/>
+
+    <div class="stat-column">
+        <div class="top">
+            <div class="title">
+                ACTIVE VALIDATORS
+            </div>
+            <div class="value">
+                {"100"}
+            </div>
+            <div class="detail">
+                {"out of 119 active bids"}
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="title">
+                CIRCULATING SUPPLY
+            </div>
+            <div class="value">
+                {"5,140,696,711"}
+            </div>
+            <div class="detail">
+                {"47.0% of 10,934,884,629"}
+            </div>
+        </div>
+    </div>
+
+    <div class="vt"/>
+
+    <div class="stat-column">
+        <div class="top">
+            <div class="title">
+                TOTAL STAKE BONDED
+            </div>
+            <div class="value">
+                {"8,255,902,991"}
+            </div>
+            <div class="detail">
+                {"75.50% of Total Supply"}
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="title">
+                TOTAL TRANSFERS
+            </div>
+            <div class="value">
+                {"8,221,2423"}
+            </div>
+        </div>
+    </div>
+
+    <div class="vt"/>
+
+    <div class="graph">
+        <!-- Graph goes here -->
+    </div>
+
+</div>
+
+<style lang='postcss'>
+    .home-stats-section {
+        @apply h-[clamp(100px,16.13vw,16.13vw)] w-[clamp(500px,92.44vw,92.44vw)];
+        @apply p-[clamp(10px,1.73vw,1.73vw)] rounded-[clamp(10px,1.19vw,1.19vw)] border-[clamp(1px,0.06vw,0.06vw)] border-[#79E1ED] border-opacity-50;
+        @apply flex gap-[clamp(10px,1.43vw,1.43vw)] items-center;
+
+        background: radial-gradient(37.71% 40.12% at 94.46% 4.58%, rgba(14, 118, 151, 0.32) 0%, rgba(12, 129, 149, 0) 100%), radial-gradient(22.05% 48.04% at 1.06% 41.67%, rgba(20, 71, 159, 0.49) 0%, rgba(19, 78, 158, 0) 100%), linear-gradient(296.24deg, rgba(22, 58, 162, 0.3) 3.67%, rgba(10, 148, 146, 0.3) 98.49%);
+        backdrop-filter: blur(7px);
+    }
+
+    .stat-column {
+        @apply w-[clamp(100px,17.86vw,17.86vw)] h-full;
+    }
+
+    .stat-column > div {
+        @apply h-1/2 pl-[clamp(20px,1.5vw,1.5vw)];
+    }
+
+    .top {
+        @apply border-b border-[#F2F3F5] border-opacity-50;
+    }
+
+    .bottom {
+        @apply mt-[clamp(8px,0.95vw,0.95vw)];
+    }
+
+    .vt {
+        @apply h-full border-r border-[#F2F3F5] border-opacity-50;
+    }
+
+    .title {
+        @apply text-[clamp(10px,0.95vw,0.95vw)] text-[#CFCFCF];
+    }
+
+    .value {
+        @apply text-[clamp(14px,1.67vw,1.67vw)] text-white font-medium;
+    }
+
+    .detail {
+        @apply text-[clamp(12px,1.07vw,1.07vw)] text-white;
+    }
+</style>
