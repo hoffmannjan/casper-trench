fc69bdb16004511255b98273d3a67f3e325f9798
HSHD <barcleodev@gmail.com>
2022-07-06 22:18:54 +0300

    refactor: background image

--- src/lib/components/Header/index.svelte
+++ src/lib/components/Header/index.svelte
@@ -9,8 +9,6 @@
 <style lang='postcss'>
     .header {
         @apply h-[30vh] sm:p-4 md:px-[clamp(16px,3.57vw,3.57vw)] md:py-[clamp(16px,0.95vw,0.95vw)];
-        @apply bg-no-repeat bg-center bg-auto;
-
-        background: url("/images/png/header-bg.png") center/cover;
+        @apply bg-[url("/images/png/header-bg.png")] bg-no-repeat bg-center bg-auto;
     }
 </style>
