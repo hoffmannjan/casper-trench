ba632d65508b4896381205e2b36eb45ad55093bc
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-06 20:59:25 +0300

    fix: header bg

--- src/lib/components/Header/index.svelte
+++ src/lib/components/Header/index.svelte
@@ -9,7 +9,8 @@
 <style lang='postcss'>
     .header {
         @apply h-[30vh] sm:p-4 md:px-[clamp(16px,3.57vw,3.57vw)] md:py-[clamp(16px,0.95vw,0.95vw)];
+        @apply bg-no-repeat bg-center bg-auto;
 
-        background: url("images/png/header-bg.png") no-repeat center center/cover;
+        background: url("/images/png/header-bg.png") center/cover;
     }
 </style>
