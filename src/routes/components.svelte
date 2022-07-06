f85ef7ea6d1eff1fc4c89c6ceda23c36aedad1d9
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-06 18:46:46 +0300

    add: top-footer component

---
+++ src/routes/components.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Footer from "$components/Footer/index.svelte";
+</script>
+
+<div class="content"></div>
+<Footer />
+
+<style lang='postcss'>
+    .content {
+        @apply h-[80vh];
+    }
+</style>
