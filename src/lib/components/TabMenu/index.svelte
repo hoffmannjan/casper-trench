ba44bff8db216262e77aace0d4a6a18b03831501
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-11 21:56:14 +0300

    add: tab menu component

---
+++ src/lib/components/TabMenu/index.svelte
@@ -0,0 +1,58 @@
+<script>
+	import TabSelectorIcon from '$lib/icons/TabSelectorIcon.svelte';
+
+	export let menuOptions = [];
+	let selected = 0;
+</script>
+
+<div class="tab-menu">
+	<div class="header">
+		{#each menuOptions as option, i}
+			<div
+				class="option"
+				class:selected={selected === i}
+				on:click={() => {
+					selected = i;
+				}}
+			>
+				{#if selected === i}
+					<div class="selector">
+						<TabSelectorIcon />
+					</div>
+				{/if}
+				<div class="text">
+					{option.title}
+				</div>
+			</div>
+		{/each}
+	</div>
+	<div class="content">
+		<svelte:component this={menuOptions[selected].component} />
+	</div>
+</div>
+
+<style lang="postcss">
+	.header {
+		@apply flex items-center;
+	}
+
+	.option {
+		@apply px-[0.6vw];
+		@apply cursor-pointer;
+	}
+
+	.text {
+		@apply px-[clamp(20px,2.14vw,2.14vw)] py-[clamp(16px,1.19vw,1.19vw)];
+	}
+
+	.selected {
+		@apply border-[clamp(1px,0.15vw,0.15vw)] border-b-0 border-color-tooltip-border border-opacity-100;
+		@apply rounded-[0.6vw] rounded-b-none;
+	}
+
+	.content {
+		@apply border-[clamp(1px,0.15vw,0.15vw)] border-color-tooltip-border border-opacity-100;
+		@apply p-[2.38vw];
+		@apply rounded-[0.6vw] rounded-tl-none;
+	}
+</style>
