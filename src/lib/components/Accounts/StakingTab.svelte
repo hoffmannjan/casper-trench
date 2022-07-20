c6ff00687e58a2b86ec59be35ce7655f6b0a398f
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-20 19:56:43 +0300

    add: empty page to staking

--- src/lib/components/Accounts/StakingTab.svelte
+++ src/lib/components/Accounts/StakingTab.svelte
@@ -1,13 +1,14 @@
 <script lang='ts'>
 	import Paginator from '$lib/components/Paginator/index.svelte';
+import EmptyIcon from '$lib/icons/EmptyIcon.svelte';
 	import { millisToFormat, timeAgo } from '$utils/converters';
     import Switch from '../Reusables/Switch.svelte';
     import TransactionStatus from '../TableData/TransactionStatus.svelte';
     import Validator from '../TableData/Validator.svelte';
 
-	// export let props = {};
-	let transactions = [
-		{
+    //sample data
+    const stakingHistory = [
+        {
 			id: '9bb2ee365c9b2672f761daac599e84c6d8ab1d25a43fba2d38e508df63ec5c79',
 			time: Date.parse('July 20, 2022 14:05'),
 			validator: {
@@ -62,7 +63,11 @@
             amount: 16324232.03423,
             status: "Success"
 		}
-	];
+    ];
+    const unstakingHistory = [];
+
+	// export let props = {};
+	let transactions = [];
 	let transactionsPerPage = 10;
 
     const switchOptions = [
@@ -81,8 +86,10 @@
 
     $: if (selected === 0) {
         //Update transactions to be staking history
+        transactions = stakingHistory;
     } else {
         //Update transactions to be unstaking history
+        transactions = unstakingHistory;
     }
 </script>
 
@@ -102,30 +109,45 @@
 			<th class="right">Status</th>
 		</tr>
 		<div class="divider table-header-border" />
-		{#each transactions as transaction}
-			<tr>
-				<td class="block">{transaction.id}</td>
-				<td><Validator hash={transaction.validator.hash} imgUrl={transaction.validator.imgUrl} name={transaction.validator.name} /></td>
-				<td class="time">{`${timeAgo(millisToFormat(Date.now() - transaction.time))} ago`}</td>
-				<td>
-                    <div class="value-crypto">
-						<div class="crypto">
-							{parseFloat(transaction.amount.toFixed(5)).toLocaleString()}
-						</div>
-						<div class="cspr">CSPR</div>
-					</div>
-                </td>
-				<td>
-					<div class="wrapper">
-                        <TransactionStatus success={transaction.status.toLowerCase() === "success"}>
-                            {transaction.status}
-                        </TransactionStatus>
-                    </div>
-                </td>
-			</tr>
-		{/each}
+		{#if transactions.length > 0}
+            {#each transactions as transaction}
+                <tr>
+                    <td class="block">{transaction.id}</td>
+                    <td><Validator hash={transaction.validator.hash} imgUrl={transaction.validator.imgUrl} name={transaction.validator.name} /></td>
+                    <td class="time">{`${timeAgo(millisToFormat(Date.now() - transaction.time))} ago`}</td>
+                    <td>
+                        <div class="value-crypto">
+                            <div class="crypto">
+                                {parseFloat(transaction.amount.toFixed(5)).toLocaleString()}
+                            </div>
+                            <div class="cspr">CSPR</div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="wrapper">
+                            <TransactionStatus success={transaction.status.toLowerCase() === "success"}>
+                                {transaction.status}
+                            </TransactionStatus>
+                        </div>
+                    </td>
+                </tr>
+            {/each}     
+        {/if}
 	</table>
-	<Paginator />
+    {#if transactions.length === 0}
+        <div class="empty">
+            <div class="content">
+                <div class="empty-icon">
+                    <EmptyIcon />
+                </div>
+                <div class="text">
+                    Empty
+                </div>
+            </div>
+        </div>
+    {:else}
+        <Paginator />
+    {/if}
 </div>
 
 <style lang="postcss">
@@ -199,5 +221,19 @@
     .switch-container {
         @apply flex justify-center;
         @apply mb-[1.31vw];
+    }
+
+    .empty-icon {
+        @apply w-[2.08vw] h-[2.08vw];
+    }
+
+    .empty {
+        @apply h-[23.81vw] w-full;
+        @apply flex items-center justify-center;
+        @apply text-[clamp(8px,1.07vw,1.07vw)] text-color-grey-footer-label text-opacity-50;
+    }
+
+    .empty > div {
+        @apply flex flex-col items-center justify-center;
     }
 </style>
