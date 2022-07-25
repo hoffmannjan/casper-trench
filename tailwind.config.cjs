88bdd33eb17318ae3b5ebc1986631be0b022124c
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-25 12:03:03 +0300

    transaction details raw data shown

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,7 +27,10 @@ module.exports = {
 					'paginator-border': '#DADADA',
 					'black-text': '#1A1919',
 					'translucent-green': '#E5F8F6',
-					'network-background': '#B8B9B9'
+					'network-background': '#B8B9B9',
+					'ago-background': '#F3F3F3',
+					'proof-num-background': '#F6F6F6',
+					'copy-btn-bg': '#F4F5F5'
 				}
 			},
 			fontFamily: {
