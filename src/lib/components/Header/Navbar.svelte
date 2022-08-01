abfdc081ea3649169c7f72aa12959e03a79c62e1
HSHD <barcleodev@gmail.com>
2022-08-01 10:42:07 +0300

    feat: navigate to sign in page

--- src/lib/components/Header/Navbar.svelte
+++ src/lib/components/Header/Navbar.svelte
@@ -95,12 +95,12 @@
 				{/if}
 			</div>
 		{/each}
-		<div class="signin">
+		<a href="/sign-in" class="signin">
 			<div class="user-icon">
 				<UserIcon />
 			</div>
 			<div class="text">Sign In</div>
-		</div>
+		</a>
 	</div>
 </div>
 
