78293166eace048a4a96b5e64f65878ca6e08cd6
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-08-03 16:01:49 +0300

    fix: y axis on comboChart

--- src/lib/components/Charts/New/ComboChart.svelte
+++ src/lib/components/Charts/New/ComboChart.svelte
@@ -72,19 +72,42 @@
 					y: {
 						type: 'linear',
 						display: true,
-						position: 'left',
+						position: 'right',
 						beginAtZero: false,
 						grid: {
 							drawOnChartArea: true
+						},
+						ticks: {
+							callback: function (val, index) {
+								const lookup = [
+									{ value: 1, symbol: "" },
+									{ value: 1e3, symbol: "k" },
+									{ value: 1e6, symbol: "M" },
+									{ value: 1e9, symbol: "G" },
+									{ value: 1e12, symbol: "T" },
+									{ value: 1e15, symbol: "P" },
+									{ value: 1e18, symbol: "E" }
+								];
+								const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+								var item = lookup.slice().reverse().find(function(item) {
+									return val >= item.value;
+								});
+								return item ? (val / item.value).toFixed(2).replace(rx, "$1") + item.symbol : "0";
+							}
 						}
 					},
 					y1: {
 						type: 'linear',
 						display: true,
-						position: 'right',
+						position: 'left',
 						beginAtZero: false,
 						grid: {
 							drawOnChartArea: true
+						},
+						ticks: {
+							callback: function (val, index) {
+								return `$${val.toFixed(3)}`;
+							}
 						}
 					}
 				},
